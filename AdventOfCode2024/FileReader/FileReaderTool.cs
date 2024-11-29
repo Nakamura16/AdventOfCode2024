b5448bdfa94d2652f0d553676c1baf7924384541
Nakamura16 <andre.massao.nakamura@gmail.com>
2024-11-29 15:50:10 -0300

    Created file reader and its ready for DayOne.csproj

---
+++ AdventOfCode2024/FileReader/FileReaderTool.cs
@@ -0,0 +1,13 @@
+﻿namespace FileReader;
+public class FileReaderTool
+{
+    public IReadOnlyList<string> ReadFile(string filePath)
+    {
+        var lines = File.ReadLines(filePath).ToList();
+        if (!lines.Any())
+        {
+            throw new Exception("The current file is empty.");
+        }
+        return lines;
+    }
+}
---
+++ AdventOfCode2024/Test/FileReader/FileReaderTests.cs
@@ -0,0 +1,35 @@
+﻿using FileReader;
+using FluentAssertions;
+using Xunit;
+
+namespace Test.FileReader;
+
+public class FileReaderTests
+{
+    private readonly FileReaderTool reader = new();
+    private const string filePath = "C:\\Users\\andre\\Desktop\\estudos C#\\AdventOfCode2024\\" +
+            "AdventOfCode2024\\Test\\FileReader\\TestFile.txt";
+    private const string emptyFilePath = "C:\\Users\\andre\\Desktop\\estudos C#\\AdventOfCode2024\\" +
+        "AdventOfCode2024\\Test\\FileReader\\TestEmptyFile.txt";
+
+    [Fact]
+    public void ReadFile_WithExistingFullFilledTextFile_ShouldReadLineByLineCorrectly()
+    {
+        var result = reader.ReadFile(filePath);
+
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Count.Should().Be(7);
+        var expectedResult = File.ReadLines(filePath);
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Fact]
+    public void ReadFile_WithExistingEmptyTextFile_ShouldThrowException()
+    {
+        reader.Invoking(r => r.ReadFile(emptyFilePath))
+            .Should()
+            .ThrowExactly<Exception>()
+            .WithMessage("The current file is empty.");
+    }
+}
